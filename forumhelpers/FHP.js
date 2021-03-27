630e87e960efd3fe8ec56fa629b6309aac3b4293
9gr <67440879+FunctionalMetatable@users.noreply.github.com>
2021-03-27 08:11:23 +0000

    Update FHP.js

--- forumhelpers/FHP.js
+++ forumhelpers/FHP.js
@@ -1,53 +1,42 @@
-var man_addition=""
-var man_pull = new XMLHttpRequest()
-
-man_pull.open("GET", "https://theforumhelpers.github.io/forumhelpers/managers.json");
-man_pull.send();
-man_pull.onreadystatechange = function() {
-	if (man_pull.readyState === 4 && man_pull.status === 200) {
-   		man_pulldone = JSON.parse(man_pull.responseText);
-		man_legnth = Object.keys(man_pulldone).length
-		for(var man_key in man_pulldone){
-    			var man_keyjson = man_pulldone[man_key];
-			var man_username = man_keyjson.name
-			var man_userID = man_keyjson.id
-			var man_bio = man_keyjson.bio
-			var man_add=
-			`<div class="forumhelpers_blocks"><h4 class="forumhelpers_name"><a href="https://scratch.mit.edu/users/${man_username}/" class="FHULink">${man_username}</a></h4>
+var man_addition = ""
+fetch("https://theforumhelpers.github.io/forumhelpers/managers.json").catch((error) => {
+	throw error
+}).then(response => response.json())
+.then((data) => {
+	let managerList = document.getElementById("FHPMan_Lists")
+	man_length = Object.keys(data).length
+	for (var man_key in data) {
+		var managerData = data[man_key]
+		var username = managerData.name
+		var userid = managerData.id
+		var bio = managerData.bio || "No biography provided."
+		var toAdd = `<div class="forumhelpers_blocks"><h4 class="forumhelpers_name"><a href="https://scratch.mit.edu/users/${username}/" class="FHULink">${username}</a></h4>
+			<a href="https://scratch.mit.edu/users/${username}/"><img src="https://uploads.scratch.mit.edu/get_image/user/${userid}_60x60.png" class="forumhelpers_pfp" loading="lazy" alt="${username}'s Profile Picture"></a>
+			<p class="forumhelpers_bio">${bio}</p>
+			<br></div>
+			<hr>`
+		man_addition += toAdd
+		managerList.innerHTML=man_addition;
+	}
+})
+var cur_addition=""
+fetch("https://theforumhelpers.github.io/forumhelpers/curators.json").catch((error) => {
+	throw error
+}).then(response => response.json())
+.then((data) => {
+	let curatorList = document.getElementById("FHPCur_Lists")
+	man_length = Object.keys(data).length
+	for (var cur_key in data) {
+		var curatorData = data[man_key]
+		var username = curatorData.name
+		var userid = curatorData.id
+		var bio = curatorData.bio || "No biography provided."
+		var toAdd = `<div class="forumhelpers_blocks"><h4 class="forumhelpers_name"><a href="https://scratch.mit.edu/users/${man_username}/" class="FHULink">${man_username}</a></h4>
 			<a href="https://scratch.mit.edu/users/${man_username}/"><img src="https://uploads.scratch.mit.edu/get_image/user/${man_userID}_60x60.png" class="forumhelpers_pfp" loading="lazy" alt="${man_username}'s Profile Picture"></a>
 			<p class="forumhelpers_bio">${man_bio}</p>
 			<br></div>
 			<hr>`
-
-				man_addition= man_addition+man_add
-				document.getElementById("FHPMan_Lists").innerHTML=man_addition;
-   		}
+		cur_addition += toAdd
+		curatorList.innerHTML=cur_addition;
 	}
-}
-
-var cur_addition=""
-var cur_pull = new XMLHttpRequest()
-
-cur_pull.open("GET", "https://theforumhelpers.github.io/forumhelpers/curators.json");
-cur_pull.send();
-cur_pull.onreadystatechange = function() {
-  	if (cur_pull.readyState === 4 && cur_pull.status === 200) {
-   		cur_pulldone = JSON.parse(cur_pull.responseText);
-		cur_legnth = Object.keys(cur_pulldone).length
-		for(var cur_key in cur_pulldone){
-    			var cur_keyjson = cur_pulldone[cur_key];
-			var cur_username = cur_keyjson.name
-			var cur_userID = cur_keyjson.id
-			var cur_bio = cur_keyjson.bio
-			var cur_add=
-			`<div class="forumhelpers_blocks"><h4 class="forumhelpers_name"><a href="https://scratch.mit.edu/users/${cur_username}/" class="FHULink">${cur_username}</a></h4>
-			<a href="https://scratch.mit.edu/users/${cur_username}/"><img src="https://uploads.scratch.mit.edu/get_image/user/${cur_userID}_60x60.png" class="forumhelpers_pfp" loading="lazy" alt="${cur_username}'s Profile Picture"></a>
-			<p class="forumhelpers_bio">${cur_bio}</p>
-			<br></div>
-			<hr>`
-
-			cur_addition= cur_addition+cur_add
-			document.getElementById("FHPCur_Lists").innerHTML=cur_addition;
-		}
-	}
-}
+})
