1c15b7cd78b43a531936b77a3aa85808630cc0bc
BarelySmooth <83728060+BarelySmooth@users.noreply.github.com>
2021-06-10 16:19:24 +0400

    Add option to switch to system theme
    The "Change Theme" button can now be used to change to system theme. Now, there are three theme options: system, light and dark.
    
    Pages other than the home page are also now linked to theme.js

--- resources/imports.js
+++ resources/imports.js
@@ -1,25 +1,3 @@
-// // Theme
-// var theme = localStorage.getItem("siteTheme");
-
-// if (theme == null){
-// 	if (window.matchMedia('(prefers-color-scheme: dark)').matches){
-// 		theme = "dark";
-// 	}
-// 	else {
-// 		theme = "light";
-// 	}
-// }
-
-// if (theme == "light") {
-// 	document.getElementsByTagName("html")[0].className = "lightMode";
-// }
-// else {
-// 	document.getElementsByTagName("html")[0].className = "darkMode";
-// }
-
-// var themeButton = document.getElementById("changeTheme");
-// themeButton.innerHTML = "Change Theme (" + theme + ")";
-
 function reference(link) {
 	const headerContent = `
 	<a href="${link}" target="_parent">
@@ -74,17 +52,3 @@ function denyPrivacy() {
 	localStorage.removeItem("FHacceptedDate");
 	window.location.href = "https://scratch.mit.edu/studios/3688309/";
 }
-
-// function changeTheme() {
-// 	var theme = localStorage.getItem("siteTheme");
-// 	console.log(theme)
-// 	if (theme == "light" || theme == null) {
-// 		theme = "dark";
-// 	}
-// 	else {
-// 		theme = "light";
-// 	}
-// 	localStorage.setItem("siteTheme", theme)
-// 	document.getElementsByTagName("html")[0].className = theme + "Mode";
-// 	themeButton.innerHTML = "Change Theme (" + theme + ")";
-// }
--- resources/theme.js
+++ resources/theme.js
@@ -1,33 +1,66 @@
 var theme = localStorage.getItem("siteTheme");
 var themeButton = document.getElementById("changeTheme");
-themeButton.innerHTML = "Change Theme (" + theme + ")";
 
 if (theme == null){
+	theme = "system";
+}
+
+themeButton.innerHTML = "Change Theme (" + theme + ")";
+
+function checkSystemTheme() {
 	if (window.matchMedia('(prefers-color-scheme: dark)').matches){
-		theme = "dark";
+		return "dark";
 	}
 	else {
-		theme = "light";
+		return "light";
 	}
 }
 
-if (theme == "light") {
+if (theme == "system") {
+	var systemTheme = checkSystemTheme();
+	if (systemTheme == "light") {
+		document.getElementsByTagName("html")[0].className = "lightMode";
+	}
+	else {
+		document.getElementsByTagName("html")[0].className = "darkMode";
+	}
+} 
+else if (theme == "light") {
 	document.getElementsByTagName("html")[0].className = "lightMode";
-}
+} 
 else {
 	document.getElementsByTagName("html")[0].className = "darkMode";
 }
 
+
 function changeTheme() {
+	// Currently Available themes: system, light, dark
+	// If you are adding a theme, please update the comment above and the array below
 	var theme = localStorage.getItem("siteTheme");
-	console.log(theme)
-	if (theme == "light" || theme == null) {
-		theme = "dark";
+	var listOfThemes = ["system", "light", "dark"];
+	var indexOfCurrentTheme = listOfThemes.indexOf(theme);
+	console.log(theme);
+	
+	if (indexOfCurrentTheme < listOfThemes.length - 1){
+		theme = listOfThemes[indexOfCurrentTheme + 1];
 	}
 	else {
-		theme = "light";
+		theme = listOfThemes[0];
 	}
+
 	localStorage.setItem("siteTheme", theme)
-	document.getElementsByTagName("html")[0].className = theme + "Mode";
+
+	if (theme == "system"){
+		var systemTheme = checkSystemTheme();
+		if (systemTheme == "light") {
+			document.getElementsByTagName("html")[0].className = "lightMode";
+		}
+		else {
+			document.getElementsByTagName("html")[0].className = "darkMode";
+		}
+	}
+	else {
+		document.getElementsByTagName("html")[0].className = theme + "Mode";
+	}
 	themeButton.innerHTML = "Change Theme (" + theme + ")";
 }
