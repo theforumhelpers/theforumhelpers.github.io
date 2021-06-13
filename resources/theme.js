theforumhelpers/theforumhelpers.github.io

commit 34aa1692bd7468fba221e0d76ce55e6077faaf22

Improve Theme Switch Interface (#226)
- Added system theme preference
- Move theme changing JS to a dedicated JS file
- Make the link to change the theme look more like a button
- Add three new CSS variables for tertiary colours 
- Make it easier to add new themes in the future

Co-authored-by: -Accio- <48496940+Accio1@users.noreply.github.com>

resources/theme.js
@@ -1,0 +1,53 @@
+var theme = localStorage.getItem("siteTheme");
+var themeButton = document.getElementById("changeTheme");
+
+if (theme == null){
+	theme = "system";
+}
+
+function checkSystemTheme() {
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+		return "dark";
+	}
+	else {
+		return "light";
+	}
+}
+
+if (theme == "system") {
+	setTheme("system")
+}
+else {
+	setTheme(theme)
+}
+
+function changeTheme() {
+	// Currently Available themes: system, light, dark
+	// If you are adding a theme, please update the comment above and the array below
+	var theme = localStorage.getItem("siteTheme");
+	var listOfThemes = ["system", "light", "dark"];
+	var indexOfCurrentTheme = listOfThemes.indexOf(theme);
+
+	if (indexOfCurrentTheme < listOfThemes.length - 1){
+		theme = listOfThemes[indexOfCurrentTheme + 1];
+	}
+	else {
+		theme = listOfThemes[0];
+	}
+
+	if (theme == "system"){
+			setTheme("system")
+	}
+	else {
+		setTheme(theme)
+	}
+}
+
+function setTheme(theme) {
+	localStorage.setItem("siteTheme", theme)
+	themeButton.innerHTML = `Change Theme (${theme})`;
+	if (theme == "system") {
+		var theme = checkSystemTheme()
+	}
+	document.getElementsByTagName("html")[0].className = theme + "Mode";
+}

404.html
@@ -27,5 +27,6 @@
 			<a href="/" class="taco" alt="Magic Taco" title="Click me!"><img src="resources/Taco-wizard.svg"></a>
 		</div>
 		<script src="resources/imports.js" onload="reference('./');"></script>
+		<script src="resources/theme.js"></script>
 	</body>
 </html>

contributors/index.html
@@ -45,6 +45,7 @@
 
 		<footer id="footer"></footer>
 		<script src="../resources/imports.js" onload='reference("../")'></script>
+		<script src="../resources/theme.js"></script>
 		<script>
 			var contributors = ["Accio1", "leahcimto", "NilsTheBest", "Nambaseking01", "shefwerld", "FunctionalMetatable", "ShadowOfTheFuture", "Shluffy", "bremea", "gosoccerboy5", "TheColaber", "awesome-llama", "CodeGuy92", "jeffalo", "MTM828", "The64thCucumber"]
 

forumhelpers/index.html
@@ -63,6 +63,7 @@
 
 	<footer id="footer"></footer>
 	<script src="../resources/imports.js" onload='reference("../")'></script>
+	<script src="../resources/theme.js"></script>
 	<script src="FHP.js"></script>
 	</body>
 </html>

index.html
@@ -39,5 +39,6 @@
 
 		<footer id="footer"></footer>
 		<script src="resources/imports.js" onload='reference("")'></script>
+		<script src="resources/theme.js"></script>
 	</body>
 </html>

privacy/index.html
@@ -37,12 +37,13 @@
 			<ul>
 				<li>Aside from Google Analytics, this site also stores two cookies on your computer which save whether or not you have agreed to the use of Google Analytics, and the date that you agreed.</li><br>
 				<li>This cookie expires each month, so you will have to re-accept the use of Google Analytics when you visit our site in a month that you have not yet accepted.</li><br>
-				<li>We also store one cookie which does not expire that stores your theme choice (light mode vs dark mode).</li><br>
+				<li>We also store one cookie which does not expire that stores your theme choice.</li><br>
 				<li>Questions? Feel free to contact us by <a href="https://github.com/theforumhelpers/theforumhelpers.github.io/discussions/new?category=q-a">opening a discussion</a> on our GitHub repository.</li>
 			</ul>
 		</div>
 
 		<footer id="footer"></footer>
 		<script src="../resources/imports.js" onload='reference("../")'></script>
+		<script src="../resources/theme.js"></script>
 	</body>
 </html>

resources/imports.js
@@ -1,11 +1,3 @@
-var theme = localStorage.getItem("siteTheme");
-if (theme == "light" || theme == null) {
-	document.getElementsByTagName("html")[0].className = "lightMode";
-}
-else {
-	document.getElementsByTagName("html")[0].className = "darkMode";
-}
-
 function reference(link) {
 	const headerContent = `
 	<a href="${link}" target="_parent">
@@ -26,7 +18,7 @@ function reference(link) {
 	const footerContent = `
 	<div class="footerContent">
 		<br>
-		<a onclick="changeTheme()" class="changeTheme">Change Theme</a><br>
+		<a onclick="changeTheme()" class="changeTheme" id="changeTheme">Change Theme</a><br>
 		<p><a href="${link}contributors/" class="FHULink" target="_parent">Contributors</a></p>
 		<p><a href="https://github.com/theforumhelpers/theforumhelpers.github.io" class="FHULink" target="_parent">Github Repository</a></p>
 		<p>Be moist <img src="https://cdn.scratch.mit.edu/scratchr2/static/__4f1f321e080ee4987f163566ecc0dd26__/djangobb_forum/img/smilies/cool.png"></p>
@@ -59,17 +51,4 @@ function denyPrivacy() {
 	localStorage.removeItem("FHacceptedAnalytics");
 	localStorage.removeItem("FHacceptedDate");
 	window.location.href = "https://scratch.mit.edu/studios/3688309/";
-}
-
-function changeTheme() {
-	var theme = localStorage.getItem("siteTheme");
-	console.log(theme)
-	if (theme == "light" || theme == null) {
-		theme = "dark";
-	}
-	else {
-		theme = "light";
-	}
-	localStorage.setItem("siteTheme", theme)
-	document.getElementsByTagName("html")[0].className = theme + "Mode";
 }

resources/stylesheet.css
@@ -10,6 +10,9 @@ html.lightMode {
 	--linkHover: blue;
 	--searchBar: white;
 	--saturation: 100%;
+	--tertiary: #2f7299;
+	--tertiaryHover: #2c566e;
+	--textTertiary: #ffffff;
 }
 
 html.darkMode {
@@ -21,6 +24,8 @@ html.darkMode {
 	--linkHover: #a1c6ff;
 	--searchBar: #1f1f1f;
 	--saturation: 60%;
+	--tertiary: #3d4042;
+	--tertiaryHover: #27292c;
 }
 
 body {
@@ -413,11 +418,21 @@ hr {
 	line-height: 10px;
 	height: auto;
 }
-.changeTheme {
-	display: block;
+
+#changeTheme {
+	background-color: var(--tertiary);
+	text-decoration: none;
+	margin: 10px;
+	padding: 20px;
+	border-style: hidden;
+	border-radius: 10px;
+	display: inline-block;
+	user-select: none; /* Repeatedly cliking the button should not select it */
 }
 
-.changeTheme:hover {
+#changeTheme:hover {
 	cursor: pointer;
+	color: var(--textTertiary);
+	background-color: var(--tertiaryHover);
 }
 /* /Footer */

commit f4d386df3dc7fb624194d09527aa008615545bec

Fix 404 page styling
Fixes a bug introduced in #226

resources/theme.js
@@ -45,7 +45,9 @@ function changeTheme() {
 
 function setTheme(theme) {
 	localStorage.setItem("siteTheme", theme)
-	themeButton.innerHTML = `Change Theme (${theme})`;
+	if (themeButton !== null) {
+		themeButton.innerHTML = `Change Theme (${theme})`;
+	}
 	if (theme == "system") {
 		var theme = checkSystemTheme()
 	}